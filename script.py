d3562714e941a3aae53b1aaf84f52014426c3c26
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-13 20:43:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,21 @@
-pipeline {
-    agent any
-    stages {
+import os
+import sys
+import shutil
 
-        stage('Clone') {
-            steps {
-                // Get some code from a GitHub repository
-                git branch: 'main', credentialsId: 'office-git-creds', url: 'https://git.rezo.ai/abhishek.khandelwal/script.py.git'
-            }
-        }
 
-        stage('Run Python Script') {
-            steps {
-                sh 'python3 script.py'
-            }
-        }
-    }
-}
+
+# Specify the Jenkins home directory
+jenkins_home = '/var/lib/jenkins'
+
+# Get a list of all build folders
+build_folders = [f.path for f in os.scandir(f"{jenkins_home}/jobs") if f.is_dir()]
+
+# Loop through each build folder and get its size
+for build_folder in build_folders:
+    build_size = 0
+    for dirpath, dirnames, filenames in os.walk(build_folder):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            build_size += os.path.getsize(fp)
+    build_size_in_mb = build_size / (1024*1024)
+    print(f"{build_folder} = ({build_size_in_mb:.2f} MB)")
